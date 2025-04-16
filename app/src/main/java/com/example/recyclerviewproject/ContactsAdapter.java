f9acd3e107ca10c4d0e8915454cd8edc0eef32fc
AryanMardoukhi <aryanmardokhi2021@gmail.com>
2025-04-16 16:45:21 +0330

    add edit contact add remove contact

--- app/src/main/java/com/example/recyclerviewproject/ContactsAdapter.java
+++ app/src/main/java/com/example/recyclerviewproject/ContactsAdapter.java
@@ -1,39 +1,55 @@
 package com.example.recyclerviewproject;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
-    private  String[] contacts=new String[20];
+import java.util.ArrayList;
 
-    public  ContactsAdapter() {
-        contacts[0]="Ruthann Trustrie";
-        contacts[1]="Peadar Dawtrey";
-        contacts[2]="Felipe Bradtke";
-        contacts[3]="Claude Crissil";
-        contacts[4]="Jacky Girardeau";
-        contacts[5]="Rubia Dominguez";
-        contacts[6]="Michaela Churchley";
-        contacts[7]="Harvey Pentelow";
-        contacts[8]="Neilla Langton";
-        contacts[9]="Marco Greaves";
-        contacts[10]="Liz Batchley";
-        contacts[11]="Lamond Littlepage";
-        contacts[12]="Malina Weir";
-        contacts[13]="Tomlin Lenchenko";
-        contacts[14]="Hy Pavelin";
-        contacts[15]="Jenelle Palin";
-        contacts[16]="Damon Knewstubb";
-        contacts[17]="Alex Ivanusyev";
-        contacts[18]="Hamil Callery";
-        contacts[19]="Karol Syer";
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
+    private static final ArrayList<String> contacts = new ArrayList<>();
+private static ItemEventListener itemEventListener;
+    public  ContactsAdapter(ItemEventListener itemEventListener) {
+        this.itemEventListener=itemEventListener;
+        contacts.add("Ruthann Trustrie");
+        contacts.add("Peadar Dawtrey");
+        contacts.add("Felipe Bradtke");
+        contacts.add("Claude Crissil");
+        contacts.add("Jacky Girardeau");
+        contacts.add("Rubia Dominguez");
+        contacts.add("Michaela Churchley");
+        contacts.add("Harvey Pentelow");
+        contacts.add("Neilla Langton");
+        contacts.add("Marco Greaves");
+        contacts.add("Liz Batchley");
+        contacts.add("Lamond Littlepage");
+        contacts.add("Malina Weir");
+        contacts.add("Tomlin Lenchenko");
+        contacts.add("Hy Pavelin");
+        contacts.add("Jenelle Palin");
+        contacts.add("Damon Knewstubb");
+        contacts.add("Alex Ivanusyev");
+        contacts.add("Hamil Callery");
+        contacts.add("Karol Syer");
 
     }
+
+    public void addNewContact (String fullname) {
+        contacts.add(0,fullname);
+        notifyItemInserted(0);
+    }
+
+    public void updateContact (String fullname , int position) {
+        contacts.set(position,fullname);
+        notifyItemChanged(position);
+    }
+
 
     @NonNull
     @Override
@@ -45,27 +61,46 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
 
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
-         holder.bindContact(contacts[position]);
+        holder.bindContact(contacts.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return contacts.length;
+        return contacts.size();
     }
 
-    public  class ContactsViewHolder extends RecyclerView.ViewHolder {
-    private  TextView firstCharactertv;
-    private  TextView fullnametv;
+    public class ContactsViewHolder extends RecyclerView.ViewHolder {
+        private final TextView firstCharactertv;
+        private final TextView fullnametv;
+
 
         public ContactsViewHolder(@NonNull View itemView) {
             super(itemView);
             firstCharactertv=itemView.findViewById(R.id.tv_contact_firstCharacter);
-            fullnametv=itemView.findViewById(R.id.tv_contact_firstCharacter);
+            fullnametv=itemView.findViewById(R.id.tv_contact_fullname);
         }
 
         public void bindContact (String fullname) {
             fullnametv.setText(fullname);
             firstCharactertv.setText(fullname.substring(0,1));
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemEventListener.onItemClick(fullname,getAdapterPosition());
+                }
+            });
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    contacts.remove(getAdapterPosition());
+                    notifyItemRemoved (getAdapterPosition());
+                    return false;
+                }
+            });
         }
+    }
+    public  interface ItemEventListener {
+        void onItemClick(String fullname , int position);
     }
 }
--- app/src/main/java/com/example/recyclerviewproject/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.recyclerviewproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +13,51 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  implements ContactsAdapter.ItemEventListener {
+private ContactsAdapter adapter;
+private int editingItemPosition = -1;
+private EditText fullnameEt;
+private ImageView addNewContactBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
 
         RecyclerView recyclerView = findViewById(R.id.rv_main);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
-        recyclerView.setAdapter(new ContactsAdapter());
+        adapter=new ContactsAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+        fullnameEt = findViewById(R.id.et_main_contactFullname);
+        addNewContactBtn=findViewById(R.id.iv_main_addNewContact);
+        addNewContactBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fullnameEt.length()>0) {
+                    if(editingItemPosition>-1) {
+                        adapter.updateContact(fullnameEt.getText().toString(),editingItemPosition);
+                        editingItemPosition=-1;
+                        addNewContactBtn.setImageResource(R.drawable.ic_add_white_24dp);
+
+                    }else {
+                        adapter.addNewContact(fullnameEt.getText().toString());
+                        recyclerView.smoothScrollToPosition(0);
+
+                    }
+                    fullnameEt.setText("");
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(String fullname, int position) {
+        editingItemPosition=position;
+        fullnameEt.setText(fullname);
+        addNewContactBtn.setImageResource(R.drawable.ic_done_white_24dp);
+
     }
 }
